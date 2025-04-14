dfb7b8e72b993dfbc576a0178e689b79991865c5
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-04-14 16:19:10 -0700

    Remove neo-tree settings

--- lua/plugins/customization.lua
+++ lua/plugins/customization.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "right",
-      },
-    },
-  },
-  {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
